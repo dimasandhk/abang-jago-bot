ae1c07b22055ef113cd4bc5d6e8d7895967184e0
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-11-26 20:49:54 +0700

    Add Image "Bantu" Handler

--- handler/bantu.js
+++ handler/bantu.js
@@ -4,16 +4,25 @@ const listBantu = [
 	{
 		name: "Minigame",
 		list: ["tebakgambar", "asahotak", "siapaku", "susunkata", "tekateki"]
+	},
+	{
+		name: "Utilitas",
+		list: ["kbbi", "wikipedia", "translate", "covidworld"]
 	}
 ];
+
+const url =
+	"https://cdn.discordapp.com/app-icons/911923037136363550/dfb2cbed846cb792d4cf2460eedcb9f0.png?size=512";
 
 const handleBantu = (msg) => {
 	const embed = new MessageEmbed({
 		title: "Bang Jago Command List",
 		color: "#23e2cf",
-		description:
-			"`bg <namaCommand>` untuk menggunakan command\n`ksana.in/reportjago` untuk report bug"
-	}).setTimestamp();
+		description: "`bg <namaCommand>` untuk menggunakan command",
+		footer: "`ksana.in/reportjago` untuk report bug "
+	})
+		.setTimestamp()
+		.setThumbnail(url);
 
 	listBantu.forEach(({ name, list }) => {
 		let strList = "";
